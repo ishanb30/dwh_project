d3034ec4f5d55e4c58b72f8fd24966f425e60200
Ishan Baddipudi <ish37b@gmail.com>
2026-01-13 13:21:19 +0000

    Change scope of script

--- sql/bronze_load_orchestration_exec.sql
+++ sql/bronze_load_orchestration_exec.sql
@@ -1,33 +1,29 @@
 /*
 ====================================
-Execute Bronze Load Orchestration
+Return Last Run From Run Log Table
 ====================================
 Script Purpose:
-This script executes the defined master stored procedure that is
-responsible for the execution of individual bronze layer stored
-procedures.
-
-Note:
-This script initially logged each ETL step using two rows (STARTED 
-and SUCCESS). The design was intentionally changed to a state-focused
-logging model to simplify querying and reduce log volume.
+This script retrieves the most recent ETL execution records from 
+the log table for the bronze layer. The retrieved data can be 
+consumed by Python for orchestration and further processing.
 */
 
-USE Datawarehouse;
-GO
-
-EXEC bronze.load_bronze_all;
-GO
-
 -- Checks whether the execution has succeeded
-SELECT
-    id, run_id, layer, proc_name, status, error_class, error_message, run_timestamp, query_run_time_ms, 
-    rows_read, rows_written
-FROM (
-    SELECT 
+WITH runs AS (
+    SELECT
         *,
-        MIN(run_timestamp) OVER (PARTITION BY run_id) AS batch_start_time
-    FROM 
+        MIN(run_timestamp) OVER(PARTITION BY run_id) AS batch_start_time
+    FROM
         admin.etl_run_log
-) AS sq
-ORDER BY batch_start_time DESC;
+)
+
+SELECT
+    *
+FROM
+    runs
+WHERE batch_start_time = (
+    SELECT MAX(batch_start_time) FROM runs
+)
+ORDER BY
+    run_timestamp
+;
