5e1498e36f00566d0c772895780eef0b96497c57
Ishan Baddipudi <ish37b@gmail.com>
2026-02-07 23:37:15 +0000

    Changed character lengths allowed based on data profiling

--- sql/bronze/create_tables.sql
+++ sql/bronze/create_tables.sql
@@ -15,58 +15,58 @@ GO
 -- Create tables from CRM data source
 IF OBJECT_ID ('bronze.crm_cust_info', 'U') IS NULL
 CREATE TABLE bronze.crm_cust_info (
-    cust_id NVARCHAR(50),
-    cust_key NVARCHAR(50),
-    cst_firstname NVARCHAR(50),
-    cst_lastname NVARCHAR(50),
-    cst_marital_status NVARCHAR(50),
-    cst_gndr NVARCHAR(50),
-    cst_create_date NVARCHAR(50)
+    cust_id NVARCHAR(12),
+    cust_key NVARCHAR(17),
+    cst_firstname NVARCHAR(45),
+    cst_lastname NVARCHAR(45),
+    cst_marital_status NVARCHAR(5),
+    cst_gndr NVARCHAR(5),
+    cst_create_date NVARCHAR(20)
 );
 
 IF OBJECT_ID ('bronze.crm_prd_info', 'U') IS NULL
 CREATE TABLE bronze.crm_prd_info (
-    prd_id NVARCHAR(50),
-    prd_key NVARCHAR(50),
+    prd_id NVARCHAR(8),
+    prd_key NVARCHAR(23),
     prd_nm NVARCHAR(50),
-    prd_cost NVARCHAR(50),
-    prd_line NVARCHAR(50),
-    prd_start_dt NVARCHAR(50),
-    prd_end_dt NVARCHAR(50)
+    prd_cost NVARCHAR(12),
+    prd_line NVARCHAR(5),
+    prd_start_dt NVARCHAR(20),
+    prd_end_dt NVARCHAR(20)
 );
 
 IF OBJECT_ID ('bronze.crm_sales_details', 'U') IS NULL
 CREATE TABLE bronze.crm_sales_details (
-    sls_order_num NVARCHAR(50),
-    sls_prd_key NVARCHAR(50),
-    sls_cust_id NVARCHAR(50),
-    sls_order_dt NVARCHAR(50),
-    sls_ship_dt NVARCHAR(50),
-    sls_due_dt NVARCHAR(50),
-    sls_sales NVARCHAR(50),
-    sls_quantity NVARCHAR(50),
-    sls_price NVARCHAR(50)
+    sls_order_num NVARCHAR(12),
+    sls_prd_key NVARCHAR(17),
+    sls_cust_id NVARCHAR(10),
+    sls_order_dt NVARCHAR(20),
+    sls_ship_dt NVARCHAR(20),
+    sls_due_dt NVARCHAR(20),
+    sls_sales NVARCHAR(12),
+    sls_quantity NVARCHAR(10),
+    sls_price NVARCHAR(12)
 );
 
 -- Create tables from ERP data source
 IF OBJECT_ID ('bronze.erp_cust_az12', 'U') IS NULL
 CREATE TABLE bronze.erp_cust_az12 (
-    cid NVARCHAR(50),
-    bdate NVARCHAR(50),
-    gen NVARCHAR(50)
+    cid NVARCHAR(18),
+    bdate NVARCHAR(20),
+    gen NVARCHAR(10)
 );
 
 IF OBJECT_ID ('bronze.erp_loc_a101', 'U') IS NULL
 CREATE TABLE bronze.erp_loc_a101 (
-    cid NVARCHAR(50),
-    cntry NVARCHAR(50)
+    cid NVARCHAR(16),
+    cntry NVARCHAR(30)
 );
 
 IF OBJECT_ID ('bronze.erp_px_cat_g1v2', 'U') IS NULL
 CREATE TABLE bronze.erp_px_cat_g1v2 (
-    id NVARCHAR(50),
-    cat NVARCHAR(50),
-    subcat NVARCHAR(50),
-    maintenance NVARCHAR(50)
+    id NVARCHAR(10),
+    cat NVARCHAR(30),
+    subcat NVARCHAR(35),
+    maintenance NVARCHAR(5)
 );
 
