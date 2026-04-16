9ad828237325c7a38156f623d1a242aad566a3cb
Ishan Baddipudi <ish37b@gmail.com>
2026-04-16 16:35:35 +0100

    Updated comment header block

--- sql/gold/dim_product.sql
+++ sql/gold/dim_product.sql
@@ -9,30 +9,19 @@ to load the transformed data into the gold dim_product table.
 
 Joins sub-dim table (erp_px_cat_g1v2) to the main dim (crm_prd_info)
 as a star schema approach is being used.
-Transformations are performed in stages using CTEs and include:
-
-- Joining the sub-dim tables to the main dim table
-- Transforming anything that requires business logic or can only be done once the
-  tables are joined. This includes:
-    - combining the gender attribute columns
 
 Assumptions:
 
 1. Foreign Keys
-   The foreign keys used to join all three tables were cst_key from crm_cust_info,
-   cid from erp_cust_az12 and cid from erp_loc_a101. These were all not selected 
-   in the final dim_customer table as they no longer served a purpose. customer_id
-   is the FK target from the fact_sales table, and since cst_key is the same as
-   customer_id, but with a prefix of 'AW000', customer_id is the only key that is
-   needed.
+   The foreign keys used to join the main and sub-dim where cat_id from
+   crm_prd_info and id from erp_px_cat_g1v2. They are not included in the
+   dim_product table because they serve no purpose beyond joining the two dim
+   tables together.
 
-2. Gender Attribute
-    There are two gender columns, cst_gndr from crm_cust_info and gen from
-    erp_cust_az12. It is decided that cst_gndr would take precedence as the column
-    for gender in the final gold table as it is from the main dim table. However, 
-    if both columns had different values then cst_gndr would be converted to NULL,
-    and if cst_gndr was already a NULL and gen had a value, then cst_gndr would
-    borrow that value.
+   However, product_key is left in dim_product because it is part of the condition
+   used to join gold.dim_product to silver.crm_sales_details. Although product_id
+   uniquely identifies the grain, product_key is useful in identifying the product
+   without considering different iterations.
 */
 
 USE DataWarehouse
--- sql/gold/fact_sales.sql
+++ sql/gold/fact_sales.sql
@@ -7,32 +7,39 @@ Purpose:
 Creates a child stored procedure, that will be called from a master procedure
 to load the transformed data into the gold fact_sales table.
 
-Joins dim table (dim) to the main dim (crm_prd_info)
-as a star schema approach is being used.
+Joins dim table (dim_customer) to the silver table (crm_sales_details)
+to introduce product_id into the fact_sales table. This is because product_id
+uniquely identifies the grain in the dim_product table, so will be used as the
+foreign key that references product_id from the dim_product table. In dim_product
+product_key can have duplicates if there are different iterations of the same
+product, so it is unreliale as a foreign key.
 Transformations are performed in stages using CTEs and include:
 
-- Joining the sub-dim tables to the main dim table
+- Joining the dim_producty to the fact table
 - Transforming anything that requires business logic or can only be done once the
   tables are joined. This includes:
-    - combining the gender attribute columns
+    - adding columns to flag date logic errors
+    - borrowing order dates from values in the same order number window. This is
+    done because all order lines in an order have to have the same order date. This
+    wasn't done for ship date or delivery date, because order lines in an order number
+    can have differing ship and delivery dates.
 
 Assumptions:
 
-1. Foreign Keys
-   The foreign keys used to join all three tables were cst_key from crm_cust_info,
-   cid from erp_cust_az12 and cid from erp_loc_a101. These were all not selected 
-   in the final dim_customer table as they no longer served a purpose. customer_id
-   is the FK target from the fact_sales table, and since cst_key is the same as
-   customer_id, but with a prefix of 'AW000', customer_id is the only key that is
-   needed.
+1. Flag Columns
+   Two more flag columns were introduced to this table. 'err_date_lifecycle' flags
+   rows where the order date is not within the range of the product start and end date.
+   'err_date_sequence' flags rows where the order_date <= ship_date <= delivery_date
+   is false. This is because order date cannot be after ship date and delivery date cannot
+   before ship date. A flag is used for both scenarios so financial data isn't compromised,
+   but it allows downstream analytics to be aware that the date logic is bad quality.
 
-2. Gender Attribute
-    There are two gender columns, cst_gndr from crm_cust_info and gen from
-    erp_cust_az12. It is decided that cst_gndr would take precedence as the column
-    for gender in the final gold table as it is from the main dim table. However, 
-    if both columns had different values then cst_gndr would be converted to NULL,
-    and if cst_gndr was already a NULL and gen had a value, then cst_gndr would
-    borrow that value.
+2. Borrowing order_date Values
+    In any case where order_date is NULL, the order date from values in the same order
+    number window is used. It is assumed that all order lines in an order number have to
+    have the same order date. However, this logic is not employed for NULLs in ship_date or
+    delivery_date because order lines in a single order number can have different ship or
+    delivery dates based on how the supply chain system is set up.
 */
 
 USE DataWarehouse
@@ -60,13 +67,13 @@ BEGIN
             silver.crm_sales_details sd
         LEFT JOIN
             gold.dim_product dp
-                ON sd.sls_prd_key = dp.product_id
+                ON sd.sls_prd_key = dp.product_key
                 AND sd.sls_order_dt BETWEEN dp.start_date AND ISNULL(dp.end_date, '9999-12-31')
     ),
     fact_sales_transformed AS(
         SELECT
             sls_ord_num AS order_number,
-            prd_id AS product_id,
+            product_id,
             sls_prd_key AS product_key,
             sls_cust_id AS customer_id,
             CASE
@@ -81,7 +88,7 @@ BEGIN
             sls_price AS price,
             sls_incomplete_financial_data AS is_incomplete_financial_data,
             CASE
-                WHEN prd_id IS NULL THEN 'Y'
+                WHEN product_id IS NULL THEN 'Y'
                 ELSE 'N'
             END AS err_date_lifecycle
         FROM
@@ -116,5 +123,7 @@ BEGIN
         *
     FROM
         fact_sales_data_chronology
+    ORDER BY
+        order_number
 END
 ;
