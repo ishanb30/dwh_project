aaea288aa26b9b74cd061a6c6776ca8dba8595a3
Ishan Baddipudi <ish37b@gmail.com>
2026-05-03 18:57:04 +0100

    Added python/ as a file path

--- python/paths.py
+++ python/paths.py
@@ -17,7 +17,10 @@ SILVER_LOAD_CHECK = SQL_SILVER_DIR / "load_orchestration_check.sql"
 
 SQL_GOLD_DIR = BASE_DIR / "sql" / "gold"
 GOLD_LOAD_CHECK = SQL_GOLD_DIR / "load_orchestration_check.sql"
+CHECK_FLAG_PCT = SQL_GOLD_DIR / "check_flag_pct.sql"
 
 SOURCE_CSV_DIR = BASE_DIR / "source"
 
 CONFIG_PATH = BASE_DIR / "python" / "config.yaml"
+
+PYTHON_DIR = BASE_DIR / "python"
